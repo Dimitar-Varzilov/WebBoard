bb6d4a40f735b121eb28fe7e5a72787d013d2495
Dimitar Varzilov <dimitar_varzilov@hotmail.com>
2025-10-16 20:03:39 +0300

    refactor: restructure Job record to improve readability and maintainability

--- WebBoard.API/Common/Models/Job.cs
+++ WebBoard.API/Common/Models/Job.cs
@@ -1,10 +1,34 @@
 using WebBoard.API.Common.Enums;
+using Sieve.Attributes;
 
 namespace WebBoard.API.Common.Models
 {
-	public record Job(Guid Id, string JobType, JobStatus Status, DateTimeOffset CreatedAt, DateTimeOffset? ScheduledAt = null)
-	{
-		public ICollection<TaskItem> Tasks { get; init; } = [];
-		public Report? Report { get; init; }
-	}
+    public record Job
+    {
+        public Guid Id { get; init; }
+
+        [Sieve(CanSort = true, CanFilter = true)]
+        public string JobType { get; init; } = string.Empty;
+
+        [Sieve(CanSort = true, CanFilter = true)]
+        public JobStatus Status { get; init; }
+
+        [Sieve(CanSort = true)]
+        public DateTimeOffset CreatedAt { get; init; }
+
+        [Sieve(CanSort = true, CanFilter = true)]
+        public DateTimeOffset? ScheduledAt { get; init; }
+
+        public ICollection<TaskItem> Tasks { get; init; } = [];
+        public Report? Report { get; init; }
+
+        public Job(Guid id, string jobType, JobStatus status, DateTimeOffset createdAt, DateTimeOffset? scheduledAt = null)
+        {
+            Id = id;
+            JobType = jobType;
+            Status = status;
+            CreatedAt = createdAt;
+            ScheduledAt = scheduledAt;
+        }
+    }
 }
